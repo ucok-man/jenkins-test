b268e04fe36f4a754db0e92e526b20c6f72f19b6
ucok-man <ucokkocu411@gmail.com>
2024-01-17 15:41:57 +0700

    add main

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello From World")
+	})
+
+	http.ListenAndServe(":8080", mux)
 }
